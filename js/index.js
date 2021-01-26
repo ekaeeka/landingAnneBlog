82716d59e0ddaafcb762235d94d6d23ec546bdee
ekaeeka <59056001+ekaeeka@users.noreply.github.com>
2021-01-26 11:35:48 +0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,16 +1,45 @@
+// function readMore() {
+//     var dots=document.getElementById("dots")
+//     var more = document.getElementById("more")
+//     var btn = document.getElementById("btn")
+//
+//     if (dots.style.display==="none") {
+//         dots.style.display="inline"
+//         btn.innerHTML = "Показать ещё"
+//         more.style.display="none"
+//     } else {
+//         dots.style.display="none"
+//         btn.innerHTML = "Скрыть"
+//         more.style.display="inline"
+//     }
+// }
 function readMore() {
-    var dots=document.getElementById("dots")
-    var more = document.getElementById("more")
-    var btn = document.getElementById("btn")
-
-    if (dots.style.display==="none") {
-        dots.style.display="inline"
-        btn.innerHTML = "Показать ещё"
-        more.style.display="none"
+    let btn = document.getElementById("btn")
+    let moduleBlockMore = document.getElementById('moduleBlockMore');
+    if (getComputedStyle(moduleBlockMore).display == 'none') {
+        moduleBlockMore.style.display = 'block';
+        btn.innerHTML = 'Скрыть';
     } else {
-        dots.style.display="none"
-        btn.innerHTML = "Скрыть"
-        more.style.display="inline"
+        moduleBlockMore.style.display = 'none';
+        btn.innerHTML = 'Показать ещё';
     }
 }
+
+function selectTheme() {
+    let buttonSelect = document.getElementById('buttonSelect');
+    let filterSelect = document.getElementById('filterSelect');
+    if (getComputedStyle(filterSelect).display == 'none') {
+        buttonSelect.style.borderBottomLeftRadius = '0';
+        buttonSelect.style.borderBottomRightRadius = '0';
+        filterSelect.style.display = 'block';
+
+    } else {
+        buttonSelect.style.borderBottomLeftRadius = '10px';
+        buttonSelect.style.borderBottomRightRadius = '10px';
+        filterSelect.style.display = 'none';
+    }
+}
+
+
+
 
